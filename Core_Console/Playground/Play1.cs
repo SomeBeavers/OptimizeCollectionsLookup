9d8bf75cc48842f9f6a986e7513257b5b12f6159
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-02-23 17:21:17 +0100

    Update.

---
+++ Core_Console/Playground/Play1.cs
@@ -0,0 +1,42 @@
+﻿namespace Core_Console.Playground;
+
+public class Play1
+{
+    private readonly Dictionary<AnimalType, Animal> _animals = new();
+    public Animal currentAnimal;
+
+    public Play1()
+    {
+        _animals.Add(AnimalType.cat, new Animal("cat"));
+        _animals.Add(AnimalType.dog, new Animal("dog"));
+
+        if (_animals.ContainsKey(AnimalType.beaver))
+        {
+            var newBeaver = new Animal("beaver");
+            //_animals.Add(AnimalType.beaver, newBeaver);
+            currentAnimal = _animals[AnimalType.beaver];
+
+            currentAnimal.Name = newBeaver.Name;
+        }
+    }
+
+    //public string GetName() {
+    //}
+}
+
+public enum AnimalType
+{
+    cat,
+    dog,
+    beaver
+}
+
+public class Animal
+{
+    public Animal(string name)
+    {
+        Name = name;
+    }
+
+    public string Name { get; set; }
+}
