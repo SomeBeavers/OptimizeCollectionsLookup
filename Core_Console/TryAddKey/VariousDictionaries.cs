f0f3615d4653c12ca87bb749d14f8575e0415efd
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-02-24 17:11:43 +0100

    Add different IDictionaries

--- Core_Console/TryAddKey/VariousDictionaries.cs
+++ Core_Console/TryAddKey/VariousDictionaries.cs
@@ -1,46 +1,114 @@
-﻿using System.Collections.Immutable;
+﻿using System.Collections;
+using System.Collections.Concurrent;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 
-namespace Core_Console.TryAddKey
+namespace Core_Console.TryAddKey;
+
+public class VariousDictionaries
 {
-    public class VariousDictionaries
+    private readonly IDictionary<int, string> _dictionary = new Dictionary<int, string>();
+
+    public void Test()
     {
-        IDictionary<int, string> _dictionary = new Dictionary<int, string>();
-
-        public void Test()
+        if (!_dictionary.ContainsKey(1))
         {
-            if (!_dictionary.ContainsKey(1)) {
-                _dictionary.Add(1, "1");
-            }
+            _dictionary.Add(1, "1");
+        }
 
 
-            SortedList<int, int> list = new();
+        SortedList<int, int> list = new();
 
-            if (!list.ContainsKey(1)) {
-                list.Add(1, 1);
-            }
+        if (!list.ContainsKey(1))
+        {
+            list.Add(1, 1);
+        }
 
-            if (list.ContainsKey(1))
-            {
-                var s = list[1];
-            }
+        if (list.ContainsKey(1))
+        {
+            var s = list[1];
+        }
 
-            var immutableDictionary = ImmutableDictionary<int, int>.Empty;
+        var immutableDictionary = ImmutableDictionary<int, int>.Empty;
 
-            if (!immutableDictionary.ContainsKey(1))
-            {
-                var _immutableDictionary = immutableDictionary.Add(1, 1);
-            }
+        if (!immutableDictionary.ContainsKey(1))
+        {
+            var _immutableDictionary = immutableDictionary.Add(1, 1);
+        }
 
-            OrderedDictionary orderedDictionary = new();
+        OrderedDictionary orderedDictionary = new();
 
-            if (orderedDictionary.Contains(1)) {
-                var s = orderedDictionary[1];
-            }
+        if (orderedDictionary.Contains(1))
+        {
+            var s = orderedDictionary[1];
+        }
 
-            if (!orderedDictionary.Contains(1)) {
-                orderedDictionary.Add(1, "1");
-            }
+        if (!orderedDictionary.Contains(1))
+        {
+            orderedDictionary.Add(1, "1");
+        }
+
+
+        HybridDictionary hybridDictionary = new();
+        if (!hybridDictionary.Contains(1))
+        {
+            hybridDictionary.Add(1, "1");
+        }
+
+        if (hybridDictionary.Contains(1))
+        {
+            var s = hybridDictionary[1];
+        }
+
+        ListDictionary listDictionary = new();
+
+        if (!listDictionary.Contains(1)) {
+            listDictionary.Add(1, "1");
+        }
+
+        if (listDictionary.Contains(1)) {
+            var s = listDictionary[1];
+        }
+
+        ReadOnlyDictionary<int, string> readOnlyDictionary = new(new Dictionary<int, string>());
+
+        if (!readOnlyDictionary.ContainsKey(1)) {
+            readOnlyDictionary.TryAdd(1, "1");
+        }
+
+        if (readOnlyDictionary.ContainsKey(1)) {
+            var s = readOnlyDictionary[1];
+        }
+
+        SortedDictionary<int, string> sortedDictionary = new();
+
+        if (!sortedDictionary.ContainsKey(1)) {
+            sortedDictionary.Add(1, "1");
+        }
+
+        if (sortedDictionary.ContainsKey(1)) {
+            var s = sortedDictionary[1];
+        }
+
+        ConcurrentDictionary<int, string> concurrentDictionary = new();
+
+        if (!concurrentDictionary.ContainsKey(1)) {
+            concurrentDictionary.TryAdd(1, "1");
+        }
+
+        if (concurrentDictionary.ContainsKey(1)) {
+            var s = concurrentDictionary[1];
+        }
+
+        Hashtable hashtable = new();
+
+        if (!hashtable.ContainsKey(1)) {
+            hashtable.Add(1, "1");
+        }
+
+        if (hashtable.ContainsKey(1)) {
+            var s = hashtable[1];
         }
     }
 }
