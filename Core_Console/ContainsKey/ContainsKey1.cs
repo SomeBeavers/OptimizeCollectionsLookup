87776500c8c14dc4cb4724b57d13ce30be379c9f
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-02-28 15:25:44 +0100

    Add ContainsKey test.

---
+++ Core_Console/ContainsKey/ContainsKey1.cs
@@ -0,0 +1,18 @@
+﻿namespace Core_Console.ContainsKey;
+
+public class ContainsKey1
+{
+    public void Test(int key, int value)
+    {
+        Dictionary<int, int> d = new();
+
+        if (d.ContainsKey(key))
+        {
+            d[key] = value;
+        }
+        else
+        {
+            d.Add(key, value);
+        }
+    }
+}
---
+++ Framework_Lib/ContainsKey/ContainsKey1.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace Framework_Lib.ContainsKey
+{
+    public class ContainsKey1
+    {
+        private Dictionary<int, int> _d = new Dictionary<int, int>();
+
+        public void Test()
+        {
+            if (_d.ContainsKey(1)) {
+                _d[1] = 1;
+            }
+            else {
+                _d.Add(1, 1);
+            }
+        }
+    }
+}
