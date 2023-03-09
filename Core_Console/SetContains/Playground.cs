506b4e6f66fbe2e4893e09dba7c86769d64807a3
lilia shamsutdinova <lilia.shamsutdinova@jetbrains.com>
2023-03-09 14:10:38 +0100

    Update.

---
+++ Core_Console/SetContains/Playground.cs
@@ -0,0 +1,23 @@
+﻿namespace Core_Console.SetContains;
+
+public class Playground<T>
+{
+    public void Test(SortedSet<T>? mySet, T? key)
+    {
+        // comment
+
+        if (mySet is not null && (key is { } && !mySet.Contains(key)))
+        {
+            // 1
+            //mySet.Add(key);
+            mySet.Add(key);
+            // 2
+
+            // 3
+        }
+        //else
+        //{
+        //    Console.WriteLine("null!!");
+        //}
+    }
+}
---
+++ Core_Console/TryGetValue/Class2.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Concurrent;
+
+namespace Core_Console1;
+
+public class VariousIDictionaries
+{
+    public void Test1(ConcurrentDictionary<MyClass, MyAnotherClass?> dictionary, MyClass? myClass)
+    {
+        Test2(dictionary);
+        if (myClass != null && dictionary.TryGetValue(myClass, out var VALUE))
+        {
+            var s = (VALUE!.ToString() ?? "");
+        }
+    }
+
+    private void Test2(IDictionary<MyClass, MyAnotherClass?> dictionary)
+    {
+        
+    }
+}
+
+public class MyAnotherClass
+{
+}
+
+public class MyClass
+{
+}
